bfbcd2541ba45514bbdcad3c1be88fa36a2eaa3a
Emil Hatlelid <emil@Emils-MacBook-Pro.local>
2019-03-04 11:13:46 -0600

    expire status on calendar event end, solution provided by @jmcmains

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ app.post('/', (req, res, next) => {
   slack.users.profile.set({
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
-      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`
+      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
+      "status_expiration": end.unix() //setting the expiration time for the status
     })
   });
   res.status(200);
